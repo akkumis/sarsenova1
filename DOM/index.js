5f6b062c81820913cf54885f05b9ef16eee3f450
akkumis <96519738+akkumis@users.noreply.github.com>
2022-02-02 16:31:43 +0600

    Add files via upload

---
+++ DOM/index.js
@@ -0,0 +1,13 @@
+function multiplyBy()
+    {
+        num1 = document.getElementById("FN").value;
+        num2 = document.getElementById("SN").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+    }
+
+    function divideBy()
+    {
+        num1 = document.getElementById("FN").value;
+        num2 = document.getElementById("SN").value;
+        document.getElementById("result").innerHTML = num1 / num2;
+    }
